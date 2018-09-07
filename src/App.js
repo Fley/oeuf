e03771a8e45b4da5ed90709d869fd1c1d10623e8
Fley <flo.eysseric@gmail.com>
2018-09-07 18:11:58 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import Theme from './components/theme/Theme';
 import Shell from './components/shell/Shell';
 import ExerciseListPageContainer from './components/exercise-list/ExerciceListPageContainer';
+import ExercisePageContainer from './components/exercise/ExercisePage';
 import RootContainer from './redux/RootContainer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <RootContainer>
-        <Theme>
-          <Shell>
-            <ExerciseListPageContainer />
-          </Shell>
-        </Theme>
+        <Router>
+          <Theme>
+            <Shell>
+              <Switch>
+                <Route exact path="/" component={ExerciseListPageContainer} />
+                <Route path="/:id" component={({ match }) => <ExercisePageContainer id={match.params.id} />} />
+                <Route component={NoMatch} />
+              </Switch>
+            </Shell>
+          </Theme>
+        </Router>
       </RootContainer>
     );
   }
