e1ba576c8a7f14cca344d7cf088c66a08c96e2bc
Fley <flo.eysseric@gmail.com>
2018-09-10 14:31:07 +0200

    Refactor 404

--- src/components/empty-page/EmptyPage.stories.js
+++ src/components/empty-page/EmptyPage.stories.js
@@ -4,27 +4,30 @@ import EmptyPage from './EmptyPage';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import { faListAlt } from '@fortawesome/free-regular-svg-icons';
+import Error404 from './Error404';
 
-storiesOf('EmptyPage', module).add('empty', () => (
-  <div>
-    <nav className="navbar sticky-top navbar-light shadow-sm" style={{ backgroundColor: 'var(--poussin)' }}>
-      Exercises
-    </nav>
-    <div className="col-sm-10 mx-auto mt-2 mb-5">
-      <EmptyPage
-        text="You have no exercises yet"
-        icon={faListAlt}
-        action={<button className="btn btn-sm btn-info">Create your first exercice</button>}
-      />
+storiesOf('EmptyPage', module)
+  .add('empty', () => (
+    <div>
+      <nav className="navbar sticky-top navbar-light shadow-sm" style={{ backgroundColor: 'var(--poussin)' }}>
+        Exercises
+      </nav>
+      <div className="col-sm-10 mx-auto mt-2 mb-5">
+        <EmptyPage
+          text="You have no exercises yet"
+          icon={faListAlt}
+          action={<button className="btn btn-sm btn-info">Create your first exercice</button>}
+        />
+      </div>
+      <nav className="navbar fixed-bottom navbar-light bg-white p-0 navbar-expand shadow-up-sm">
+        <ul className="d-flex navbar-nav w-100">
+          <li className="nav-item flex-grow-1">
+            <a className="nav-link" href="">
+              <FontAwesomeIcon icon={faPlusSquare} /> NEW EXERCISE
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
-    <nav className="navbar fixed-bottom navbar-light bg-white p-0 navbar-expand shadow-up-sm">
-      <ul className="d-flex navbar-nav w-100">
-        <li className="nav-item flex-grow-1">
-          <a className="nav-link" href="">
-            <FontAwesomeIcon icon={faPlusSquare} /> NEW EXERCISE
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-));
+  ))
+  .add('Error 404', () => <Error404 />);
--- src/components/empty-page/Error404.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Error404 from './Error404';
-
-storiesOf('Error404', module).add('404', () => <Error404 />);
