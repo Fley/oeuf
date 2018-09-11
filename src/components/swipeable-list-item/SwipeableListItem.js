a53e4eacdbbef919e5bf2fb2957e7f6f46123af5
Fley <flo.eysseric@gmail.com>
2018-09-12 00:43:21 +0200

    Small fix on swipeable list wip

--- src/components/swipeable-list-item/SwipeableListItem.js
+++ src/components/swipeable-list-item/SwipeableListItem.js
@@ -1,44 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faCheck, faUndo } from '@fortawesome/free-solid-svg-icons';
 
-const SwipeableListItem = ({ className, children, onSwipeRight, onSwipeLeft, rightSwipeElement, leftSwipeElement }) => {
-  let onTransitionEnd;
-  const onChangeIndex = (index, indexLatest, meta) => {
-    onTransitionEnd = index === 0 ? onSwipeLeft : index === 2 ? onSwipeRight : null;
-  };
-  return (
-    <li className={className}>
-      <SwipeableViews
-        index={1}
-        onTransitionEnd={() => onTransitionEnd && onTransitionEnd()}
-        onChangeIndex={onChangeIndex}
-      >
-        {leftSwipeElement}
-        {children}
-        {rightSwipeElement}
-      </SwipeableViews>
-    </li>
-  );
-};
+export default class SwipeableListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { index: 1 };
+  }
 
-export default SwipeableListItem;
+  onChangeIndex = (index, indexLatest, meta) => {
+    this.setState({ index });
+  };
+
+  onTransitionEnd = () => {
+    if (this.state.index === 0) {
+      this.props.onSwipeLeft();
+    } else if (this.state.index === 2) {
+      this.props.onSwipeRight();
+    }
+    this.setState({ index: 1 });
+  };
+
+  render() {
+    const { className, children, rightSwipeElement, leftSwipeElement } = this.props;
+
+    return (
+      <li className={className}>
+        <SwipeableViews
+          index={this.state.index}
+          onTransitionEnd={this.onTransitionEnd}
+          onChangeIndex={this.onChangeIndex}
+        >
+          {leftSwipeElement}
+          {children}
+          {rightSwipeElement}
+        </SwipeableViews>
+      </li>
+    );
+  }
+}
 
 export const SwipedItemAcknowledged = () => (
-  <div className="d-flex bg-success text-white p-2 h-100 justify-content-end">
+  <div className="d-flex bg-success text-white py-2 px-3 h-100 justify-content-end">
     <FontAwesomeIcon icon={faCheck} className="align-self-center" />
   </div>
 );
 
 export const SwipedItemRemoved = () => (
-  <div className="bg-danger text-white p-2 h-100">
+  <div className="bg-danger text-white py-2 px-3 h-100">
     <FontAwesomeIcon icon={faTimes} className="align-self-center" />
   </div>
 );
 
 export const SwipedItemCanceled = () => (
-  <div className="d-flex bg-warning text-white p-2 h-100 justify-content-end">
+  <div className="d-flex bg-warning text-white py-2 px-3 h-100 justify-content-end">
     <FontAwesomeIcon icon={faUndo} className="align-self-center" />
   </div>
 );
