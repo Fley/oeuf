f607ff061cb24a4ecacfc2c62be2984e4e8b1567
Fley <flo.eysseric@gmail.com>
2019-01-21 23:41:57 +0100

    remove unused code

--- custom-sw.js
+++ custom-sw.js
@@ -1,18 +1,3 @@
 self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
-
-// self.addEventListener('message', event => {
-//   if (!event.data) {
-//     return;
-//   }
-
-//   switch (event.data) {
-//     case 'skipWaiting':
-//       self.skipWaiting().then(() => {});
-//       break;
-//     default:
-//       // NOOP
-//       break;
-//   }
-// });
